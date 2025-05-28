c6e42f53a27bad5f8ab09471fd1ce0258afb9f77
xzilliazia <mziaulhaq05@gmail.com>
2025-05-28 21:12:54 +0700

    -

--- LibrarySystem/src/main/java/com/librarysystem/MainApp.java
+++ LibrarySystem/src/main/java/com/librarysystem/MainApp.java
@@ -6,18 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/librarysystem/hello-view.fxml"));
-        Parent root = fxmlLoader.load(); // Load the FXML
-        Scene scene = new Scene(root, 800, 600); // Create the scene with the loaded root
-        primaryStage.setTitle("Library System");
-        primaryStage.setScene(scene); // Set the scene to the stage
-        primaryStage.show(); // Show the window
+public class MainApp {
+    public static void main(String[] args) {
+
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
 }
