c5b4d8a285ae14e40b9881b1acc7a78e6e295477
xzilliazia <mziaulhaq05@gmail.com>
2025-06-03 21:58:16 +0700

    update 3

--- LibrarySystem/src/main/java/com/librarysystem/books/Book.java
+++ LibrarySystem/src/main/java/com/librarysystem/books/Book.java
@@ -4,10 +4,11 @@ public class Book {
     private String bookId, title, author, category;
     private int stock;
 
-    public Book(String bookId, String title, String author, int stock) {
+    public Book(String bookId, String title, String author,String category, int stock) {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
+        this.category = category;
         this.stock = stock;
     }
     public String getBookId() {
--- LibrarySystem/src/main/java/com/librarysystem/books/FantasyBook.java
+++ LibrarySystem/src/main/java/com/librarysystem/books/FantasyBook.java
@@ -3,8 +3,8 @@ package com.librarysystem.books;
 public class FantasyBook extends Book {
     private String category = "Fantasy";
     public FantasyBook(String bookId, String title, String author, int stock) {
-        super(bookId, title, author, stock);
-        super.setCategory(category);
+        super(bookId, title, author, "Fantasy", stock);
+
     }
     @Override
     public String getCategory() {
