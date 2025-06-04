4642f33e4573e89df3a04513e68e3541369713cc
xzilliazia <mziaulhaq05@gmail.com>
2025-06-04 23:43:48 +0700

    delete

--- LibrarySystem/src/main/java/com/librarysystem/util/PMenu.java
+++
@@ -1,7 +0,0 @@
-package com.librarysystem.util;
-
-import javafx.stage.Stage;
-
-public interface PMenu {
-    public void menu(Stage stage);
-}
--- LibrarySystem/src/main/java/module-info.java
+++ LibrarySystem/src/main/java/module-info.java
@@ -6,9 +6,11 @@ module com.librarysystem {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires java.logging;
 
     opens com.librarysystem to javafx.fxml;
     exports com.librarysystem;
     exports com.librarysystem.controller;
+    exports com.librarysystem.UI;
     opens com.librarysystem.controller to javafx.fxml;
 }
