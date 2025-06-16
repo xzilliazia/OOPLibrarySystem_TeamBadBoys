0a6b79f549b6dfe3db7eb598051b2723f2f47334
AkmalJecky <akmalforza1234@gmail.com>
2025-06-16 22:01:51 +0700

    update login user

--- LibrarySystem/src/main/java/com/librarysystem/controller/LoginController.java
+++ LibrarySystem/src/main/java/com/librarysystem/controller/LoginController.java
@@ -1,40 +1,59 @@
-//package com.librarysystem.controller;
-//
-//import javafx.scene.control.Alert;
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//
-//public class LoginController {
-//
-//    public enum userType {
-//        MAHASISWA, ADMIN, INVALID
-//    }
-//
-//    public userType validatedUser(String input){
-//
-//    }
-//
-//    public boolean validatedAdminPassword(String codeAdmin, String password){
-//        //load data
-//        //readline
-//        //Exception
-//    }
-//
-//    public boolean isMahasiswa(String nim){
-//        //load data
-//        //readline
-//        //exception
-//    }
-//
-//    public boolean isAdminCode(String code){
-//        //same as the other
-//    }
-//
-//    private void showAlert(String message){
-//        Alert alert = new Alert(Alert.AlertType.ERROR);
-//        alert.setHeaderText("Eror");
-//        alert.setContentText(message);
-//        alert.showAndWait();
-//    }
-//}
+package com.librarysystem.controller;
+
+import com.librarysystem.util.DatabaseConnection;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+
+public class LoginController {
+
+    public enum Role {
+        MAHASISWA, ADMIN, INVALID
+    }
+
+    public Role login(String usernameOrNim, String password){
+        if(isAdmin(usernameOrNim, password)){
+            return Role.ADMIN;
+        } else if (isMahasiswa(usernameOrNim, password)) {
+            return Role.MAHASISWA;
+        } else {
+            return Role.INVALID;
+        }
+    }
+
+    private boolean isAdmin(String username, String password){
+        String query = "SELECT * FROM admin WHERE username=? AND password=?";
+        try(Connection conn = DatabaseConnection.connect();
+            PreparedStatement stmt = conn.prepareStatement(query))
+        {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                return rs.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean isMahasiswa(String nim, String password){
+        String query = "SELECT * FROM mahasiswa WHERE nim = ? AND password = ?";
+        try(Connection conn = DatabaseConnection.connect();
+            PreparedStatement stmt = conn.prepareStatement(query))
+        {
+            stmt.setString(1, nim);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                return rs.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
