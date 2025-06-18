334673c0d581e0e947b6d3ab6012fd82938c4a04
xzilliazia <mziaulhaq05@gmail.com>
2025-06-18 23:49:22 +0700

    del-useless files

--- LibrarySystem/src/main/java/com/librarysystem/controller/controller.java
+++
@@ -1,4 +0,0 @@
-package com.librarysystem.controller;
-
-public class controller {
-}
--- LibrarySystem/src/main/java/com/librarysystem/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.librarysystem.service;
-
-public class AuthService {
-        public static boolean authenticate(String username, String password) {
-            //validasi sementara
-            return "admin".equals(username) && "123".equals(password);
-    }
-}
